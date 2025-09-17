354b89bfd4e0e5732b65a37fe54a922b98e7da0d
chandanc5525 <chaudhari.chandan22@gmail.com>
2025-09-17 17:56:11 +0530

    Project Structure Done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="US_Visa",
+    version="1.0.0",
+    author="Chandan Chaudhari",
+    author_email="chaudhari.chandan22@gmail.com",
+    packages=find_packages()
+)
